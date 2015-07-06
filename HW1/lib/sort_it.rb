ef030bcf8f6a8be3621d2279dbd73af43f53a9b7
fkuster <fkuster@foi.hr>
2015-07-06 10:06:54 +0200

    sort solved in one line

--- HW1/lib/sort_it.rb
+++ HW1/lib/sort_it.rb
@@ -3,9 +3,5 @@
 # The length of all words provided in the list will be >= n. The format will be "x, x, x"
 
 def sort_it(list_, n)
- hash={}
- words_array=list_.split(", ")
- words_array.each{|word| hash[word]=word[n-1]}
- sorted=hash.sort_by{|k, v| v}
- sorted.map{|k,v| "#{k}"}.join(', ')
+ list_.split(", ").sort_by{|item| item[n-1]}.join(', ')
 end
