59fd4fd7b40c01698480a61aabf125114a292499
fkuster <fkuster@foi.hr>
2015-07-17 17:11:23 +0200

    user id fixed

--- reddit_homework_03/app/controllers/subscriptions_controller.rb
+++ reddit_homework_03/app/controllers/subscriptions_controller.rb
@@ -1,14 +1,14 @@
 class SubscriptionsController < ApplicationController
 
   def new
-    Subscription.create(user_id:session["warden.user.user.key"][0][0],subreddit_id:params[:subreddit_id])
+    Subscription.create(user_id:current_user.id,subreddit_id:params[:subreddit_id])
     respond_to do |format|
            format.js {}
            format.html {}
        end
   end
   def destroy
-    Subscription.find_by(user_id:session["warden.user.user.key"][0][0],subreddit_id:params[:subreddit_id]).destroy
+    Subscription.find_by(user_id:current_user.id,subreddit_id:params[:subreddit_id]).destroy
     respond_to do |format|
            format.js {}
            format.html {}
