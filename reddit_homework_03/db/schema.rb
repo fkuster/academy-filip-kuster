654d55619f7e01bfe547e35ad58e71c2d433eec4
fkuster <fkuster@foi.hr>
2015-07-21 20:49:31 +0200

    deleted author_name and creator_name from models

--- reddit_homework_03/db/schema.rb
+++ reddit_homework_03/db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721180649) do
+ActiveRecord::Schema.define(version: 20150721184328) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.integer  "post_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "author_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
 
@@ -28,7 +27,6 @@ ActiveRecord::Schema.define(version: 20150721180649) do
     t.integer  "subreddit_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.string   "creator_name"
     t.integer  "upvotes_count"
     t.integer  "comments_count"
     t.integer  "user_id"
