c3209cf83910fdd65a9158cd727e5e141853cca4
fkuster <fkuster@foi.hr>
2015-07-22 20:49:09 +0200

    fix

--- reddit_homework_03/app/controllers/api/v1/api_controller.rb
+++ reddit_homework_03/app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ApiController < RocketPants::Base
       version 1
-    
+
     end
   end
 end
---
+++ reddit_homework_03/app/serializers/comment_serializer.rb
@@ -0,0 +1,7 @@
+module Api
+  module V1
+    class CommentSerializer < ActiveModel::Serializer
+
+    end
+  end
+end
